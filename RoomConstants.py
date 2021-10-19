9f93b9e8c7a919e254eb1d85373def2f7f3009ac
wh1ter0se <camelCaseEverything@gmail.com>
2021-10-19 15:25:47 -0500

    Removed stacking from 2D grid map.

--- RoomConstants.py
+++ RoomConstants.py
@@ -15,7 +15,7 @@ class House():
 	grnd = []
 	allstrips = list(range(0,64*8))
 
-	grid_map = np.array([[-1 for x in range(32)] for y in range(128)],dtype=object)
+	grid_map = np.array([[-1 for y in range(21)] for x in range(43)],dtype=object)
 
 	def add_strip_to_grid_map(self,strip,start_pos,direction_uvec,rev):
 		if rev:
@@ -26,11 +26,16 @@ class House():
 				start_pos[1]+count*direction_uvec[1]]
 			if self.grid_map[new_pos[0]][new_pos[1]] == -1:
 				self.grid_map[new_pos[0]][new_pos[1]] = indx
-			else:
-				if not isinstance(self.grid_map[new_pos[0]][new_pos[1]],list):
-					self.grid_map[new_pos[0]][new_pos[1]] = [self.grid_map[new_pos[0]][new_pos[1]]]
-				self.grid_map[new_pos[0]][new_pos[1]].append(indx)
+			#else:
+			#	if not isinstance(self.grid_map[new_pos[0]][new_pos[1]],list):
+			#		self.grid_map[new_pos[0]][new_pos[1]] = [self.grid_map[new_pos[0]][new_pos[1]]]
+			#	self.grid_map[new_pos[0]][new_pos[1]].append(indx)
 			count += 1
+	
+	def print_grid_map(self):
+		for i in range(len(self.grid_map[0,:])):
+			row = self.grid_map[:,i]
+			print(row)
 
 	def __init__(self,init_grid_map):
 		init_grid_map()
@@ -115,20 +120,22 @@ class State(House):
 
 	def init_grid_map(self):
 		print('init grid map')
-		super().add_strip_to_grid_map(self.splashA[:19],[0,20],[0,-1],False)
+		super().add_strip_to_grid_map(self.splashA[:20],[0,20],[0,-1],False)
 		super().add_strip_to_grid_map(self.splashA[20:],[1,0],[1,0],False)
-		super().add_strip_to_grid_map(self.splashB[:19],[10,20],[0,-1],False)
-		super().add_strip_to_grid_map(self.splashB[20:26],[11,0],[1,0],False)
+		super().add_strip_to_grid_map(self.splashB[:20],[10,20],[0,-1],False)
+		super().add_strip_to_grid_map(self.splashB[20:27],[11,0],[1,0],False)
 		super().add_strip_to_grid_map(self.splashB[27:],[18,1],[0,1],False)
-		super().add_strip_to_grid_map(self.splashC[:19],[26,20],[0,-1],False)
+		super().add_strip_to_grid_map(self.splashC[:20],[26,20],[0,-1],False)
 		super().add_strip_to_grid_map(self.splashC[20:],[25,0],[-1,0],False)
-		super().add_strip_to_grid_map(self.splashD[:15],[34,20],[0,-1],False)
+		super().add_strip_to_grid_map(self.splashD[:16],[34,20],[0,-1],False)
 		super().add_strip_to_grid_map(self.splashD[16:],[33,4],[-1,0],False)
-		super().add_strip_to_grid_map(self.splashE[:15],[42,20],[0,-1],False)
+		super().add_strip_to_grid_map(self.splashE[:16],[42,20],[0,-1],False)
 		super().add_strip_to_grid_map(self.splashE[16:],[41,4],[-1,0],False)
-		super().add_strip_to_grid_map(self.desk[:14],[11,15],[0,0],False)
-		super().add_strip_to_grid_map(self.desk[15:21],[11,15],[1,0],False)
-		super().add_strip_to_grid_map(self.desk[22:],[17,15],[1,0],False)
+		#super().add_strip_to_grid_map(self.desk[:15],[11,15],[0,0],False)
+		#super().add_strip_to_grid_map(self.desk[15:22],[11,15],[1,0],False)
+		#super().add_strip_to_grid_map(self.desk[22:],[17,15],[1,0],False)
+		#print(self.grid_map[:20,:15])
+		self.print_grid_map()
 
 	def __init__(self):
 		self.splash = self.splashA + self.splashB + self.splashC + \
